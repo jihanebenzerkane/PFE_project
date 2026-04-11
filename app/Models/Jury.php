cdb08c02d50addcf25a3511accced95e926674f5
boujaadafatimazahra <boujaadafatimazahra@gmail.com>
2026-04-11 13:17:32 +0100

    add salles jurys migrations

---
+++ app/Models/Jury.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jury extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [];
+
+    public function enseignants()
+    {
+        return $this->belongsToMany(Enseignant::class, 'jury_enseignant')
+                    ->withPivot('role')
+                    ->withTimestamps();
+    }
+
+    public function soutenance()
+    {
+        return $this->hasOne(Soutenance::class);
+    }
+}
