f9481b2a4cf993d2071768bc13cd6bd1156e0432
jihanebenzerkane <jihanebenzerkane@gmail.com>
2026-04-29 01:50:11 +0100

    feat: create Etudiant model and corresponding database migration table

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model{
+ protected $table = "etudiant";
+ protected $fillable = ["id","nom","prenom","filiere"];
+}
